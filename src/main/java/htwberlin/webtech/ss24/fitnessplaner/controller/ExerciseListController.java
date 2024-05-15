6847e753b22868b4775c5ca618c0ff2d5bf5f250
VladimirWittich <vladimirwittich@web.de>
2024-05-15 18:18:48 +0200

    Anpassungen für die Kommunikation mit dem Frontend

--- src/main/java/htwberlin/webtech/ss24/fitnessplaner/controller/ExerciseListController.java
+++ src/main/java/htwberlin/webtech/ss24/fitnessplaner/controller/ExerciseListController.java
@@ -1,6 +1,5 @@
 package htwberlin.webtech.ss24.fitnessplaner.controller;
 
-
 import htwberlin.webtech.ss24.fitnessplaner.model.Exercise;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,16 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RequestMapping("/workoutplan")
-
 @CrossOrigin(origins = "https://fitnessplaner-frontend-webtech-ss24-e6t9.onrender.com")
-
 @RestController
 public class ExerciseListController {
 
     @GetMapping()
     public List<Exercise> getExercise() {
-        return List.of(
-                new Exercise("Bench", 2)
-        );
+        // Erstelle eine Übung mit Wiederholungen und Gewicht
+        Exercise exercise = new Exercise("Bench", 2, List.of(10, 12), List.of(50, 55));
+
+        // Gib eine Liste mit dieser Übung zurück
+        return List.of(exercise);
     }
 }
--- src/main/java/htwberlin/webtech/ss24/fitnessplaner/model/Exercise.java
+++ src/main/java/htwberlin/webtech/ss24/fitnessplaner/model/Exercise.java
@@ -1,6 +1,19 @@
 package htwberlin.webtech.ss24.fitnessplaner.model;
 
+import java.util.List;
+
 public record Exercise (
-    String name,
-    int sets){
+        String name,
+        int sets,
+        List<Integer> repetitions,
+        List<Integer> weight) {
+
+    // Berechnung des Gesamtgewichts
+    public int totalWeight() {
+        int totalWeight = 0;
+        for (int i = 0; i < repetitions.size(); i++) {
+            totalWeight += repetitions.get(i) * weight.get(i);
+        }
+        return totalWeight;
+    }
 }
