876647e8031d8c87d43277aa50fa22bb229f3960
VladimirWittich <vladimirwittich@web.de>
2024-05-15 17:49:14 +0200

    Anpassungen für die Kommunikation mit dem Frontend

--- src/main/java/htwberlin/webtech/ss24/fitnessplaner/model/Exercise.java
+++ src/main/java/htwberlin/webtech/ss24/fitnessplaner/model/Exercise.java
@@ -2,5 +2,8 @@ package htwberlin.webtech.ss24.fitnessplaner.model;
 
 public record Exercise (
     String name,
-    int sets){
+    int sets,
+    int repetitions,
+    double weight,
+    double totalweight){
 }
