599884fa794b72c5b8e277d14a3921c965594d14
VladimirWittich <vladimirwittich@web.de>
2024-06-07 00:24:21 +0200

    Anpassungen

--- src/main/java/htwberlin/webtech/ss24/fitnessplaner/web/WebConfig.java
+++ src/main/java/htwberlin/webtech/ss24/fitnessplaner/web/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "https://fitnessplaner-frontend-webtech-ss24-e6t9.onrender.com")
+                        .allowedOrigins("http://localhost:5173/", "https://fitnessplaner-frontend-webtech-ss24-e6t9.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
