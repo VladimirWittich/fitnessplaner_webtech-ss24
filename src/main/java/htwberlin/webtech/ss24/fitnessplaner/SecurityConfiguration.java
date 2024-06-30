46916deb552e98dd518f2a3bfd1b6c6587b01758
VladimirWittich <vladimirwittich@web.de>
2024-06-30 20:39:10 +0200

    Anpassungen im SecurityConfiguration

--- src/main/java/htwberlin/webtech/ss24/fitnessplaner/SecurityConfiguration.java
+++ src/main/java/htwberlin/webtech/ss24/fitnessplaner/SecurityConfiguration.java
@@ -20,6 +20,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeHttpRequests(auth -> {
                     auth
                             .requestMatchers("/bmicalculator").permitAll()
